d20e2b219c5e64a557a9ac86b3af9f96e60c1968
aleksas <test@test.com>
2023-07-05 17:24:51 +0300

    feat: add stub reviews-service module

---
+++ reviews-service/src/main/kotlin/ru/aasmc/reviewsservice/ReviewsServiceApplication.kt
@@ -0,0 +1,11 @@
+package ru.aasmc.reviewsservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReviewsServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<ReviewsServiceApplication>(*args)
+}
