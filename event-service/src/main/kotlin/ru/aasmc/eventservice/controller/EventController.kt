bdc551fa130e3925e5c66b5930cc3d090b54723b
Alex Alexeyev <aasmc@yandex.ru>
2023-07-10 22:54:48 +0300

    feat: add open-api docs to event-service

--- event-service/src/main/kotlin/ru/aasmc/eventservice/controller/EventController.kt
+++ event-service/src/main/kotlin/ru/aasmc/eventservice/controller/EventController.kt
@@ -1,9 +1,18 @@
 package ru.aasmc.eventservice.controller
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.ArraySchema
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import ru.aasmc.eventservice.dto.CommonEventDto
 import ru.aasmc.eventservice.service.EventService
@@ -16,7 +25,26 @@ class EventController(
         private val eventService: EventService
 ) {
 
+    @Operation(summary = "Get all events for user by his/her ID.")
+    @ApiResponses(value = [
+        ApiResponse(
+                responseCode = "200",
+                description = "Events for User",
+                content = [
+                    Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            array = (
+                                    ArraySchema(
+                                            schema = Schema(
+                                                implementation = CommonEventDto::class
+                                            ))
+                                    )
+                    )
+                ]
+        )
+    ])
     @GetMapping("/{userId}/feed")
+    @ResponseStatus(HttpStatus.OK)
     fun getEventsForUser(@PathVariable("userId") userId: Long): List<CommonEventDto> {
         log.info("Received request to GET events for user with ID={}", userId)
         return eventService.findAllEventsByUserId(userId)
--- event-service/src/main/kotlin/ru/aasmc/eventservice/dto/CommonEventDto.kt
+++ event-service/src/main/kotlin/ru/aasmc/eventservice/dto/CommonEventDto.kt
@@ -1,13 +1,37 @@
 package ru.aasmc.eventservice.dto
 
+import io.swagger.v3.oas.annotations.media.Schema
 import ru.aasmc.eventservice.enums.CommonEventOperation
 import ru.aasmc.eventservice.enums.CommonEventType
 
 data class CommonEventDto(
+        @field:Schema(
+                description = "ID of the event",
+                example = "1",
+                type = "long"
+        )
         val eventId: Long? = null,
+        @field:Schema(
+                description = "Epoc millis, specifying the moment the event happened."
+        )
         val timestamp: Long,
+        @field:Schema(
+                description = "ID of the User who initiated the event."
+        )
         val userId: Long,
+        @field:Schema(
+                description = "Specifies type of the event. FRIEND, LIKE, REVIEW",
+                example = "FRIEND"
+        )
         val eventType: CommonEventType,
+        @field:Schema(
+                description = "Specifies the operation performed when the event happened. " +
+                        "REMOVE, ADD, UPDATE, REMOVE_ALL.",
+                example = "ADD"
+        )
         val operation: CommonEventOperation,
+        @field:Schema(
+                description = "ID of entity the event happened to."
+        )
         val entityId: Long
 )
