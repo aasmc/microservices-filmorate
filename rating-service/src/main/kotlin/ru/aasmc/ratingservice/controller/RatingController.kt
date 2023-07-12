af148573ef718d564d4552f8c4e3a5532898a303
aleksas <test@test.com>
2023-07-12 10:51:10 +0300

    feat: add api docs to rating-service

--- rating-service/src/main/kotlin/ru/aasmc/ratingservice/controller/RatingController.kt
+++ rating-service/src/main/kotlin/ru/aasmc/ratingservice/controller/RatingController.kt
@@ -1,7 +1,14 @@
 package ru.aasmc.ratingservice.controller
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.ArraySchema
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
 import ru.aasmc.ratingservice.service.RatingService
 
@@ -13,6 +20,17 @@ class RatingController(
         private val ratingService: RatingService
 ) {
 
+    @Operation(summary = "Retrieves a list of IDs of Films liked by a user with specified ID.")
+    @ApiResponses(value = [
+        ApiResponse(
+                responseCode = "200",
+                description = "Success",
+                content = [Content(
+                        mediaType = MediaType.APPLICATION_JSON_VALUE,
+                        array = ArraySchema(schema = Schema(implementation = Long::class))
+                )]
+        )
+    ])
     @GetMapping("/films/{userId}")
     @ResponseStatus(HttpStatus.OK)
     fun getFilmIdsForUser(@PathVariable("userId") userId: Long): List<Long> {
@@ -22,6 +40,17 @@ class RatingController(
         return filmIds
     }
 
+    @Operation(summary = "Retrieves a rating of the film or 0.0 if DB contains no rating info.")
+    @ApiResponses(value = [
+        ApiResponse(
+                responseCode = "200",
+                description = "Success",
+                content = [Content(
+                        mediaType = MediaType.APPLICATION_JSON_VALUE,
+                        schema = Schema(implementation = Double::class)
+                )]
+        )
+    ])
     @GetMapping("/{filmId}")
     @ResponseStatus(HttpStatus.OK)
     fun getFilmRating(@PathVariable("filmId") filmId: Long): Double {
@@ -31,6 +60,17 @@ class RatingController(
         return rating
     }
 
+    @Operation(summary = "Retrieves a list of IDs of Films recommended to a user with specified ID.")
+    @ApiResponses(value = [
+        ApiResponse(
+                responseCode = "200",
+                description = "Success",
+                content = [Content(
+                        mediaType = MediaType.APPLICATION_JSON_VALUE,
+                        array = ArraySchema(schema = Schema(implementation = Long::class))
+                )]
+        )
+    ])
     @GetMapping("/user/{userId}")
     fun getRecommendedUserForUser(@PathVariable("userId") userId: Long): List<Long> {
         log.info("Received request to GET id of user recommended for user with ID={}", userId)
